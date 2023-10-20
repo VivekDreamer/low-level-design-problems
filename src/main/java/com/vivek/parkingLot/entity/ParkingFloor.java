72b12f41b65b5ae0fbebaed0b1456a203ec9c01d
VivekDreamer <vivekmakv@gmail.com>
2023-10-21 02:33:38 +0530

    Entity class is added -> ParkingFloor.java

---
+++ src/main/java/com/vivek/parkingLot/entity/ParkingFloor.java
@@ -0,0 +1,17 @@
+package com.vivek.parkingLot.entity;
+
+import java.util.List;
+
+public class ParkingFloor {
+    List<ParkingSlot> parkingSlots = null;
+    public ParkingFloor(List<ParkingSlot> parkingSlots){
+        this.parkingSlots = parkingSlots;
+    }
+    public List<ParkingSlot> getParkingSlots() {
+        return parkingSlots;
+    }
+    public void setParkingSlots(List<ParkingSlot> parkingSlots) {
+        this.parkingSlots = parkingSlots;
+    }
+     
+}
